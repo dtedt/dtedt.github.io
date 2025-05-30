da6cb43b884f54386a82380926387bc975b6c507
Ted Tarnovski <117692961+dtedt@users.noreply.github.com>
2025-05-30 11:08:50 -0400

    Update flip.js

--- grid/flip.js
+++ grid/flip.js
@@ -2,5 +2,9 @@ let cornerCard = document.getElementById("module2")
 let wrapper = document.getElementById("wrapper")
 
 function fitScreen() {
-
+    wrapper.width = window.innerWidth;
+    wrapper.height = window.innerHeight;
 }
+
+fitScreen()
+
