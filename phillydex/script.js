35fafff6339a25636ef942bd5afa29d502d95d1d
Ted Tarnovski <117692961+dtedt@users.noreply.github.com>
2026-06-01 10:19:28 -0400

    Add contacts and games data structures
    Added contact and game data structures with multiple entries.

--- phillydex/script.js
+++ phillydex/script.js
@@ -1,1 +1,44 @@
+const contacts = [
+  { id: 1, 
+   name: "Cam", 
+   email: "cam@crabs.sand", 
+   website: "www.cam.com",
+   games: "Oswego: Quest For The Golden Pin", "Terry Groose Eats Sand",
+   WIP: "untitled card game",
+   roles: "Writer", "Game Designer"
+  },
+  { id: 2, 
+   name: "Ted Tarnovski", 
+   email: "ted@dtedt.com", 
+   games: "Seer Of Stars", 
+   WIP: "Fend",
+   roles: "Producer", "QA Tester", 
+  },
+  { id: 3,
+   name: "Jovan Kouakou",
+   email: "jovan@jovankouakou.com",
+   games: "Seer Of Stars",
+   WIP: "",
+   roles: "Illustrator", "Web Designer", "SEO",
+  },
+  { id: ,
+   name: ,
+   email:,
+   games: ,
+   WIP: ,
+   roles: ,
+];
 
+
+const games = [
+  { id: 1,
+   img: 
+   title: "Oswego: Quest For The Golden Pin",
+   engine: "GBStudio",
+   platforms: "Itch.io", "Steam"
+   genre: "Top-down", "Adventure",
+   link: https://store.steampowered.com/app/4211310/Oswego_Quest_for_the_Golden_Pin/,
+   
+  }
+
+  ]
